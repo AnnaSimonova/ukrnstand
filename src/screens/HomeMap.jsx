f750f7ed5e0a4d57298196d6f137bf24555942f8
AnnaSimonova <simonova.ganna@gmail.com>
2022-03-27 23:37:42 +0200

    Change map view on resize screen added

--- src/screens/HomeMap.jsx
+++ src/screens/HomeMap.jsx
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, } from "react-leaflet";
+// import { useMapEvents } from "react-leaflet";
 
 import axios from "axios";
 
 const HomeMap = () => {
   const position = [49.3, -124.3167];  
-  let zoomValue;
-  if (window.matchMedia('(max-width: 375px)').matches) {
-    zoomValue = 0;
-  } else if (window.matchMedia('(max-width: 767px)').matches) {
-    zoomValue = 0.5;
-  } else if (window.matchMedia('(max-width: 1024px)').matches) {
-    zoomValue = 1.25;
-  } else {
-    zoomValue = 1.75;
+  const [zoomValue, changeZoomValue] = useState(setZoomValue());
+
+  function setZoomValue() {
+    let zoom;
+    if (window.matchMedia('(max-width: 375px)').matches) {
+      zoom = 0;
+    } else if (window.matchMedia('(max-width: 767px)').matches) {
+      zoom = 0.5;
+    } else if (window.matchMedia('(max-width: 1024px)').matches) {
+      zoom = 1.25;
+    } else {
+      zoom = 1.75;
+    }
+    return zoom;
   }
+
+  useEffect(() => {
+    window.addEventListener('resize', function() {
+      changeZoomValue(setZoomValue());
+    });
+  }, []);
+
   return (
     <>
       <div className="row map-wrapper">
-        <MapContainer center={[49, 31]} zoom={zoomValue}>
+        <MapContainer center={[49, 31]} zoom={zoomValue} key={zoomValue}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
