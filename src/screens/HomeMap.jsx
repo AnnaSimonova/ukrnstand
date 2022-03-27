c033f6397c4fcb7ac90b77588d769e698216b1ad
AnnaSimonova <simonova.ganna@gmail.com>
2022-03-27 15:47:10 +0200

    Adjusted mobile map view

--- src/screens/HomeMap.jsx
+++ src/screens/HomeMap.jsx
@@ -4,11 +4,21 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import axios from "axios";
 
 const HomeMap = () => {
-  const position = [49.3, -124.3167];
+  const position = [49.3, -124.3167];  
+  let zoomValue;
+  if (window.matchMedia('(max-width: 375px)').matches) {
+    zoomValue = 0;
+  } else if (window.matchMedia('(max-width: 767px)').matches) {
+    zoomValue = 0.5;
+  } else if (window.matchMedia('(max-width: 1024px)').matches) {
+    zoomValue = 1.25;
+  } else {
+    zoomValue = 1.75;
+  }
   return (
     <>
-      <div className="row">
-        <MapContainer center={[51.505, -0.09]} zoom={13}>
+      <div className="row map-wrapper">
+        <MapContainer center={[49, 31]} zoom={zoomValue}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
